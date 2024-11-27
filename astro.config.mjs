a75267d06df2f5c0362b6345673bebeecf9ef947
qrol <ggglondyn@gmail.com>
2024-11-27 13:15:32 +0100

    font fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,25 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[hash][extname]',
+        },
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/critical.css";`,
+        },
+      },
+    },
+  },
 });
